a9d7cbfe1c2bedcd4b59e3cf3e37a1eb3d314c8b
geetika25 <geetika2598@gmail.com>
2018-08-25 18:20:09 -0700

    django models

--- geetika1/settings.py
+++ geetika1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'movie1.apps.Movie1Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- movie1/models.py
+++ movie1/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    actor = models.CharField(max_length=30)
+    actor_movies = models.CharField(max_length=50)
+    genre = models.CharField(max_length=20)
+    movie_logo = models.CharField(max_length=100)
+
+
+class song(models.Model):
+     movie=models.ForeignKey(Movie, on_delete=models.CASCADE)
+     file_type = models.CharField(max_length=50)
+     song_name = models.CharField(max_length=100)
